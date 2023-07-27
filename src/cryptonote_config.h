2af72a12da8d1df6addfbd049f2acf7afec8266c
Avalyn Project <140300971+avalyn-project@users.noreply.github.com>
2023-07-27 14:08:48 -0500

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -161,11 +161,11 @@ namespace config
   uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 21;
   uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 35;
   uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 38;
-  uint16_t const P2P_DEFAULT_PORT = 38080;
-  uint16_t const RPC_DEFAULT_PORT = 38081;
-  uint16_t const ZMQ_RPC_DEFAULT_PORT = 38082;
+  uint16_t const P2P_DEFAULT_PORT = 43730;
+  uint16_t const RPC_DEFAULT_PORT = 43731;
+  uint16_t const ZMQ_RPC_DEFAULT_PORT = 43732;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x12
+      0x10 ,0x21, 0xF4, 0x02 , 0x64, 0x06 , 0x11, 0x32, 0x27, 0x13, 0x05, 0x36, 0x14, 0xA6, 0xA4, 0x16
     } }; // Bender's nightmare
   std::string const GENESIS_TX =
     "01"                                                                  // version
@@ -176,23 +176,23 @@ namespace config
     "01"                                                                  // vout length
     "ffffffffffff07"                                                      // output #1 amount
     "02"                                                                  // output 1 type
-    "0bf6522f9152fa26cd1fc5c022b1a9e13dab697f3acf4b4d0ca6950a867a1943"    // output #1 key
+    "9b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071"    // output #1 key
     "21"                                                                  // extra length in bytes
     "01"                                                                  // extra pubkey tag
-    "1d92826d0656958865a035264725799f39f6988faa97d532f972895de849496d00"; // tx pubkey
+    "c54729d8d3697db8befa853a595763ba50cf9c909af059bbeb185578d09aae5e00"; // tx pubkey
 
-  uint32_t const GENESIS_NONCE = 10000;
+  uint32_t const GENESIS_NONCE = 16301;
 
   namespace testnet
   {
     uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 37;
     uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 16;
     uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 39;
-    uint16_t const P2P_DEFAULT_PORT = 48080;
-    uint16_t const RPC_DEFAULT_PORT = 48081;
-    uint16_t const ZMQ_RPC_DEFAULT_PORT = 48082;
+    uint16_t const P2P_DEFAULT_PORT = 53730;
+    uint16_t const RPC_DEFAULT_PORT = 53731;
+    uint16_t const ZMQ_RPC_DEFAULT_PORT = 53732;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x13
+      0x10 ,0x21, 0xF4, 0x02 , 0x64, 0x06 , 0x11, 0x32, 0x27, 0x13, 0x05, 0x36, 0x14, 0xA6, 0xA4, 0x17
       } }; // Bender's daydream
   }
 
@@ -205,7 +205,7 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 58081;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 58082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x14
+      0x10 ,0x21, 0xF4, 0x02 , 0x64, 0x06 , 0x11, 0x32, 0x27, 0x13, 0x05, 0x36, 0x14, 0xA6, 0xA4, 0x18
       } }; // Bender's daydream
   }
 }
