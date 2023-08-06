3c6677bd471d9fbaf23e36f925a49fa56e3cb496
Avalyn Project <140300971+avalyn-project@users.noreply.github.com>
2023-08-06 13:50:23 -0500

    Update blockchain_usage.cpp

--- src/blockchain_utilities/blockchain_usage.cpp
+++ src/blockchain_utilities/blockchain_usage.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018, The Masari Project
+// Copyright (c) 2023, The Avalyn Project
 // Copyright (c) 2014-2018, The Monero Project
 //
 // All rights reserved.
@@ -130,12 +130,12 @@ int main(int argc, char* argv[])
 
   if (command_line::get_arg(vm, command_line::arg_help))
   {
-    std::cout << "Masari '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL << ENDL;
+    std::cout << "Avalyn '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL << ENDL;
     std::cout << desc_options << std::endl;
     return 1;
   }
 
-  mlog_configure(mlog_get_default_log_path("masari-blockchain-usage.log"), true);
+  mlog_configure(mlog_get_default_log_path("avalyn-blockchain-usage.log"), true);
   if (!command_line::is_arg_defaulted(vm, arg_log_level))
     mlog_set_log(command_line::get_arg(vm, arg_log_level).c_str());
   else
